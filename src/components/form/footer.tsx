a8b9680d83f33108cf9b44309a03d072370b8958
sebastianlee90 <159765561+sebastianlee90@users.noreply.github.com>
2025-08-18 12:27:22 +0800

    #20 : Create Footer component (#21)

---
+++ src/components/form/footer.tsx
@@ -0,0 +1,38 @@
+import { Footer as FFooter } from "@/components/ui/footer";
+// import { GitHubLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
+import { Logo } from "../icons/logo";
+
+export function Footer() {
+  return (
+    <FFooter
+      logo={<Logo className="size-10" />}
+      brandName="Web Toolbox"
+      // socialLinks={[
+      //   {
+      //     icon: <TwitterLogoIcon className="h-5 w-5" />,
+      //     href: "https://twitter.com",
+      //     label: "Twitter",
+      //   },
+      //   {
+      //     icon: <GitHubLogoIcon className="h-5 w-5" />,
+      //     href: "https://github.com",
+      //     label: "GitHub",
+      //   },
+      // ]}
+      mainLinks={[
+        // { href: "/products", label: "Products" },
+        { href: "/about", label: "About" },
+        // { href: "/blog", label: "Blog" },
+        // { href: "/contact", label: "Contact" },
+      ]}
+      legalLinks={[
+        { href: "/privacy", label: "Privacy" },
+        // { href: "/terms", label: "Terms" },
+      ]}
+      copyright={{
+        text: "© 2025 Web Toolbox",
+        license: "All rights reserved",
+      }}
+    />
+  );
+}
