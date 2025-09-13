58851d8c9274364ccc3bb59cdd95cc02f97e7725
sebastianlee90 <sebastianlkc90@gmail.com>
2025-09-13 16:18:20 +0800

    #13 : Implement Infinite Moving Cards Component. Update Homepage Design to ommit Sidebar rendering.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,25 @@ import { Faq } from "@/components/form/faq";
 import { Logo } from "@/components/icons/logo";
 import { BackgroundRippleEffect } from "@/components/ui/background-ripple-effect";
 import { CardSpotlight } from "@/components/ui/card-spotlight";
+import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
 import { FAQ_QNA } from "@/constants/faqQnA";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-20">
+      {/* Hero Section */}
       <BackgroundRippleEffect>
         <div className="flex flex-col items-center justify-center w-full z-10">
-          <h2 className="flex gap-2 relative mx-auto max-w-4xl text-center text-2xl font-bold text-foreground md:text-4xl lg:text-7xl">
+          <h1 className="flex gap-2 relative mx-auto max-w-4xl text-center text-2xl font-bold text-foreground md:text-4xl lg:text-7xl">
             <Logo className="text-foreground size-10 md:size-14 lg:size-24" />
             <span className="mt-1.5">Web Toolbox</span>
-          </h2>
+          </h1>
           <p className="relative mx-auto mt-4 max-w-xl text-center text-xs md:text-md lg:text-2xl text-muted-foreground">
             Simplifying tasks, one tool at a time
           </p>
         </div>
       </BackgroundRippleEffect>
+      {/* Quality Section */}
       {/* TODO: Finetune Content Displayed in the 3 cardspotlight section*/}
       <div className="grid grid-cols-3 items-center justify-center gap-4">
         <CardSpotlight className="h-[200px]">
@@ -39,12 +42,18 @@ export default function Home() {
           </p>
         </CardSpotlight>
       </div>
-      <div className="flex items-center justify-center h-50 border border-foreground rounded-xl m-10">
-        Featured Tools Section
+      {/* Featured Tools Section */}
+      <div className="flex flex-col gap-8">
+        <h1 className="text-center text-xl md:text-2xl lg:text-4xl">
+          🔥Featured Tools🔥
+        </h1>
+        <InfiniteMovingCards direction="left" />
+        {/* Tools Section */}
+        <div className="flex items-center justify-center h-50 border border-foreground rounded-xl m-10">
+          Tools Section
+        </div>
       </div>
-      <div className="flex items-center justify-center h-50 border border-foreground rounded-xl m-10">
-        Tools Section
-      </div>
+      {/* FAQ Section */}
       <Faq
         title="Frequently Asked Questions"
         description="Everything you need to know about our platform"
---
+++ src/components/ui/infinite-moving-cards.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import { toolsList, toolsType } from "@/constants/toolsList";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+
+export function InfiniteMovingCards({
+  direction = "left",
+  speed = "normal",
+  pauseOnHover = true,
+  className,
+}: {
+  direction?: "left" | "right";
+  speed?: "fast" | "normal" | "slow";
+  pauseOnHover?: boolean;
+  className?: string;
+}) {
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const scrollerRef = React.useRef<HTMLUListElement>(null);
+  const allTools = toolsList.reduce<toolsType[]>((acc, item) => {
+    if (item.children) {
+      return [...acc, ...item.children];
+    }
+    return acc;
+  }, []);
+
+  useEffect(() => {
+    addAnimation();
+  }, []);
+  const [start, setStart] = useState(false);
+  function addAnimation() {
+    if (containerRef.current && scrollerRef.current) {
+      const scrollerContent = Array.from(scrollerRef.current.children);
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        if (scrollerRef.current) {
+          scrollerRef.current.appendChild(duplicatedItem);
+        }
+      });
+
+      getDirection();
+      getSpeed();
+      setStart(true);
+    }
+  }
+  const getDirection = () => {
+    if (containerRef.current) {
+      if (direction === "left") {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "forwards"
+        );
+      } else {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "reverse"
+        );
+      }
+    }
+  };
+  const getSpeed = () => {
+    if (containerRef.current) {
+      if (speed === "fast") {
+        containerRef.current.style.setProperty("--animation-duration", "20s");
+      } else if (speed === "normal") {
+        containerRef.current.style.setProperty("--animation-duration", "40s");
+      } else {
+        containerRef.current.style.setProperty("--animation-duration", "80s");
+      }
+    }
+  };
+
+  return (
+    <div
+      ref={containerRef}
+      className={cn(
+        "scroller relative z-20 overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        className
+      )}
+    >
+      <ul
+        ref={scrollerRef}
+        className={cn(
+          "flex w-max min-w-full shrink-0 flex-nowrap gap-4 py-4",
+          start && "animate-scroll",
+          pauseOnHover && "hover:[animation-play-state:paused]"
+        )}
+      >
+        {allTools.map((item) => (
+          <li
+            key={item.id}
+            className={cn(
+              "relative shrink-0 rounded-2xl p-1",
+              "max-w-full w-[300px] md:w-[300px]",
+              "border-3 border-ring",
+              "bg-secondary"
+            )}
+          >
+            <Link
+              className={cn(
+                "flex flex-row p-3 gap-2 w-full h-full items-center text-nowrap",
+                "rounded-2xl border-2 border-secondary hover:border-blue-400"
+              )}
+              href={item.href}
+            >
+              {item.icon ? <item.icon className="size-8" /> : null}
+              <span className="text-foreground text-lg font-semibold">
+                {item.name}
+              </span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
